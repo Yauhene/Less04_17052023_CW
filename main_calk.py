3a3b5437882083167aa1b747aad4806d42a1d954
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-17 19:58:32 +0300

    добавили файл main.lamb.py

--- main_calk.py
+++ main_calk.py
@@ -1,1 +1,23 @@
-﻿
+﻿def calk1(a):
+    return a+a
+
+def calk2(a):
+    return a*a
+
+def calk3(a, b):
+    return a + b
+
+def calk4(a, b):
+    return a * b
+
+def math(op,x):
+    print(op(x))
+
+def math2(op,x,y):
+    print(op(x,y))
+
+
+math(calk1,5)
+math(calk2,5)
+math2(calk3, 5, 45)
+math2(calk4, 5, 45)
