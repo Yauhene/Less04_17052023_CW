8935e5a2783fe70b5eb6acb137886bbd021d97d5
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-17 23:12:56 +0300

    добавлен main_os.py

---
+++ main_files.py
@@ -0,0 +1,32 @@
+﻿# Режим a
+
+colors = ['red', 'green', 'blue']
+data = open('file_1.txt', 'a', encoding='utf-8') # здесь указываем режим, в котором будем работать
+data.writelines(colors) # разделителей не будет
+data.close()
+
+# ● data.close() — используется для закрытия файла, чтобы разорвать
+# подключение файловой переменной с файлом на диске.
+# ● exit() — позволяет не выполнять код, прописанный после этой команды в
+# скрипте.
+# ● В итоге создаётся текстовый файл с текстом внутри: redbluedreen.
+# ● При повторном выполнении скрипта redbluedreenredbluedreen — добавление
+# в существующий файл, а не перезапись файлов.
+# Ещё один способ записи данных в файл:
+
+with open('file_2.txt', 'w') as data:
+    data.write('line 1\n')
+    data.write('line 2\n')
+
+# Режим r
+path = 'file.txt'
+data = open(path, 'r')
+for line in data:
+    print(line)
+data.close()
+
+# 3. Режим w
+colors = ['red', 'green', 'blue']
+data = open('file.txt', 'w')
+data.writelines(colors) # разделителей не будет
+data.close()
---
+++ main_os.py
@@ -0,0 +1,22 @@
+﻿import os
+
+print('======== Текущая папка ======')
+currDir = os.getcwd()
+print(os.getcwd())
+
+os.chdir('D:/Jack')
+print('======== Новая текущая папка ======')
+print(os.getcwd())
+
+os.chdir(currDir)
+print('======== Возврат назад ======')
+print(os.getcwd())
+
+print()
+# os.path.basename(path) - базовое имя пути
+print(os.path.basename('D:/Jack/Java II/1_Незавершенка/Знакомство с языком программирования Python/Урок 4. Функции высшего порядка, работа с файлами/Less04_17052023_CW/main.py'))
+print()
+# os.path.abspath(path) - возвращает нормализованный абсолютный путь.
+print(os.path.abspath('main.py'))
+print()
+
