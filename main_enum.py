c5589fb72a82cbc3b5c7ef9e1dbf771c5c9274bf
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-17 21:45:19 +0300

    добавлен и написан main_enum.py

---
+++ main_enum.py
@@ -0,0 +1,5 @@
+﻿# Функция enumerate() позволяет пронумеровать набор данных.
+
+users = ['user1', 'user2', 'user3']
+data = list(enumerate(users))
+print(data) # [(0, 'user1'), (1, 'user2'), (2, 'user3))]
