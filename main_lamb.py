3f3ba6d512e43f1836a8be8427a16b97db409ef5
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-17 20:46:27 +0300

    добавили main_map.py

--- main_lamb.py
+++ main_lamb.py
@@ -1,1 +1,12 @@
-﻿
+﻿# работа с лямбда-функциями
+def calk3(a, b):
+    return a + b
+
+calk5 = lambda a,b: a + b # данная функция по сути - упрощенное объявление функции kalk3(a, b)
+
+def math2(op,x,y):
+    print(op(x,y))
+
+math2(calk3, 5, 45)
+math2(calk5, 5, 45)
+math2(lambda a,b: a + b, 5, 45) # будет работать точно так же как math2(calk3, 5, 45)
