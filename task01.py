c86e63df9711a4fc6e71c42070dd318f3be0af90
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-17 21:38:53 +0300

    добавлен и написан main_zip.py

--- task01.py
+++ task01.py
@@ -30,3 +30,11 @@ print(res)
 
 res = list(select(lambda x: (x, x**2), res))
 print(res)
+
+# ==== Упрощенный вариант с использованием map() и filter():
+
+data = [1,2,3,5,8,15,23,38]
+res = map(int, data)
+res = filter(lambda x: x % 2 == 0, res)
+res = list(map(lambda x: (x, x**2), res))
+print(res)
